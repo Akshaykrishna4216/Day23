adcea0376010523311f23ba1ea509ca2c1600e57
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-05-08 09:35:41 +0530

    Add files via upload

---
+++ ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class IntExtension
+    {
+        public static bool IsGreaterthan(this int i,int value)
+        {
+            return i > value;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            bool result=i.IsGreaterthan(5);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static string Concatenate(this string a, string b)
+        { return a + b; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello";
+            string b = " world";
+
+            string res = a.Concatenate(b);
+            Console.WriteLine(res);
+        }
+    }
+}
